63741adcb1a715ebf7417864a7c3582b857bc14f
Fabricio Richieri <fabriciorichieri@yahoo.com>
2023-10-14 23:19:02 -0300

    12, 13

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -1,23 +1,88 @@
 const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
 ];
 const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
 ];
+
+const deadline = document.querySelector('.deadline');
+const giveaway = document.querySelector('.giveaway');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+// let futureDate = new Date(2023, 9, 14, 11, 56, 0);
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 10, 0, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const month = months[futureDate.getMonth()];
+const day = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${day} ${month} ${year} ${hours}:${minutes}am`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  console.log(t);
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60min
+  /// 1d = 24hr
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000; // ms in one Day - always
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  const format = (item) => {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  };
+
+  // set values array
+  const values = [days, hours, minutes, seconds];
+
+  items.forEach((item, index) => {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(interval);
+    deadline.innerHTML = `<h4 class="expired">Sorry, this giveaway has expired</h4>`;
+  }
+}
+
+const interval = setInterval(() => {
+  getRemainingTime();
+}, 1000);
